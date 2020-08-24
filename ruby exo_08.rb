bd3b9e4cfcf93e476d3b52e2d617323d6eef5a20
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-08-24 17:07:56 +0200

    Update ruby exo_08.rb

--- ruby exo_08.rb
+++ ruby exo_08.rb
@@ -1,5 +1,3 @@
-$ ruby exo_08.rb
 print "Entrez votre nom"
 first_name = gets.chomp
-print "Bonjour," + first_name + "!"
-#j'ai choisi d'utiliser print et non puts afin déviter le retour à la ligne
+print "Bonjour, #{first_name} !"

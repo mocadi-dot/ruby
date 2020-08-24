ac49f7694464d06fdd8d13d9a2ee4fa6e12642ea
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-08-24 17:08:40 +0200

    Update ruby exo_10.rb

--- ruby exo_10.rb
+++ ruby exo_10.rb
@@ -1,7 +1,4 @@
-$ ruby exo_10.rb
 puts "Quel est ton année de naissance ?"
 born_year = gets.chomp
 puts "Tu es né en " + born_year
 puts "En 2017 tu avais #{2017 - born_year.to_i} "
-
-# mettre .to_i apres string pour le convertir en integer
